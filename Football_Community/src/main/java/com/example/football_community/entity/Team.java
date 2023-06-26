2171b391ae29ef39e347aac9680f1db939c7eefc
구환준/모건 <koojun99@naver.com>
2023-06-26 23:03:00 +0900

    Team Entity

--- Football_Community/src/main/java/com/example/football_community/entity/Team.java
+++ Football_Community/src/main/java/com/example/football_community/entity/Team.java
@@ -17,9 +17,74 @@ public class Team {
     @Column(nullable = false, unique = true)
     private String home_stadium;
 
-    @Column()
+    @Column(nullable = false)
+    private String league;
 
-    @OneToMany
-    @JoinColumn(name = "user_id")
-    private List<User> user;
+    @OneToMany(mappedBy = "team")
+    private List<User> users;
+
+    @ManyToMany(mappedBy = "teams")
+    private List<Match> matches
+
+    @OneToMany(mappedBy = "team")
+    private List<Notification> notifications;
+
+    @OneToMany(mappedBy = "team")
+    private List<TeamSchedule> teamSchedules;
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getHome_stadium() {
+        return home_stadium;
+    }
+
+    public void setHome_stadium(String home_stadium) {
+        this.home_stadium = home_stadium;
+    }
+
+    public String getLeague() {
+        return league;
+    }
+
+    public void setLeague(String league) {
+        this.league = league;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    public List<TeamSchedule> getTeamSchedules() {
+        return teamSchedules;
+    }
+
+    public void setTeamSchedules(List<TeamSchedule> teamSchedules) {
+        this.teamSchedules = teamSchedules;
+    }
 }
