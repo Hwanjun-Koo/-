324a10576cebc6cc1a38f0d4f67f3c3c2a527659
구환준/모건 <koojun99@naver.com>
2023-09-18 23:20:25 +0900

    #16 feat: Add Reviews to Newsfeed

--- Football_Community/src/main/java/com/example/football_community/domain/newsfeed/service/NewsfeedService.java
+++ Football_Community/src/main/java/com/example/football_community/domain/newsfeed/service/NewsfeedService.java
@@ -4,6 +4,9 @@ import com.example.football_community.domain.newsfeed.dto.PageDto;
 import com.example.football_community.domain.post.dto.response.PostDetailsResponseDto;
 import com.example.football_community.domain.post.entity.Post;
 import com.example.football_community.domain.post.repository.PostRepository;
+import com.example.football_community.domain.review.dto.response.ReviewDetailsResponseDto;
+import com.example.football_community.domain.review.entity.Review;
+import com.example.football_community.domain.review.repository.ReviewRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -19,17 +22,23 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class NewsfeedService {
     private final PostRepository postRepository;
+    private final ReviewRepository reviewRepository;
 
     @Transactional
     public PageDto getPosts(int page, int size){
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
         Page<Post> postPage = postRepository.findAll(pageable);
+        Page<Review> reviewPage = reviewRepository.findAll(pageable);
         boolean hasNext = postPage.hasNext();
-        List<PostDetailsResponseDto> dtos = postPage.getContent().stream()
+        List<PostDetailsResponseDto> postDtos = postPage.getContent().stream()
                 .map(PostDetailsResponseDto::of)
                 .collect(Collectors.toList());
+        List<ReviewDetailsResponseDto> reviewDtos = reviewPage.getContent().stream()
+                .map(ReviewDetailsResponseDto::of)
+                .collect(Collectors.toList());
         return PageDto.builder()
-                .posts(dtos)
+                .posts(postDtos)
+                .reviews(reviewDtos)
                 .hasNext(hasNext)
                 .build();
     }
